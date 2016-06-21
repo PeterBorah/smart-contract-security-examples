a1cce365262c480cafc5e95e81360b4e993cab56
Peter Borah <peterborah@gmail.com>
2016-06-21 15:13:20 -0500

    Create CircuitBreaker.

---
+++ contracts/CircuitBreaker.sol
@@ -0,0 +1,59 @@
+contract CircuitBreaker {
+  struct Transfer { uint amount; address to; uint releaseBlock; bool released; bool stopped; }
+  Transfer[] public transfers;
+
+  address public curator;
+  address public authorizedSender;
+  uint public period;
+  uint public limit;
+
+  uint public currentPeriodEnd;
+  uint public currentPeriodAmount;
+
+  event PendingTransfer(uint id, uint amount, address to, uint releaseBlock);
+
+  function CircuitBreaker(address _curator, address _authorizedSender, uint _period, uint _limit) {
+    curator = _curator;
+    period = _period;
+    limit = _limit;
+    authorizedSender = _authorizedSender;
+    currentPeriodEnd = block.number + period;
+  }
+
+  function transfer(uint amount, address to) {
+    if (msg.sender == authorizedSender) {
+      updatePeriod();
+
+      if (currentPeriodAmount + amount > limit) {
+        uint releaseBlock = block.number + period;
+        PendingTransfer(transfers.length, amount, to, releaseBlock);
+        transfers.push(Transfer(amount, to, releaseBlock, false, false));
+      } else {
+        currentPeriodAmount += amount;
+        transfers.push(Transfer(amount, to, block.number, true, false));
+        to.send(amount);
+      }
+    }
+  } 
+  
+  function updatePeriod() {
+    if (currentPeriodEnd < block.number) {
+      currentPeriodEnd = block.number + period;
+      currentPeriodAmount = 0;
+    }
+  }
+
+  function releasePendingTransfer(uint id) {
+    Transfer transfer = transfers[id];
+    if (transfer.releaseBlock <= block.number && !transfer.released && !transfer.stopped) {
+      transfer.released = true;
+      transfer.to.send(transfer.amount);
+    }
+  }
+  
+  function stopTransfer(uint id) {
+    if (msg.sender == curator) {
+      transfers[id].stopped = true;
+    }
+  }
+}
---
+++ test/circuit_breaker.js
@@ -0,0 +1,175 @@
+contract('CircuitBreaker', function(accounts) {
+  it("should transfer ether", function(done) {
+    var circuit_breaker;
+    var recipient;
+
+    var test = function() {
+      circuit_breaker.transfer(100, recipient.address).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 100);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 900);
+          done();
+        }).catch(done);
+    }
+
+    CircuitBreaker.new(accounts[1], accounts[0], 3, 500).
+      then(function(result) { circuit_breaker = result }).
+      then(function() { return Recipient.new(); }).
+      then(function(result) { recipient = result }).
+      then(function() { return web3.eth.sendTransaction({from: accounts[0], to: circuit_breaker.address, value: 1000}, test) }).
+      catch(done);
+  });
+
+  it("should wait the specified number of blocks to transfer more than the limit", function(done) {
+    var circuit_breaker;
+    var recipient;
+
+    var test = function() {
+      circuit_breaker.transfer(600, recipient.address).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 0);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 1000);
+        }).
+        then(function() { return circuit_breaker.releasePendingTransfer(0) }).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 0);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 1000);
+        }).
+        then(function() { return circuit_breaker.releasePendingTransfer(0) }).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 0);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 1000);
+        }).
+        then(function() { return circuit_breaker.releasePendingTransfer(0) }).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 600);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 400);
+          done();
+        }).catch(done);
+    }
+
+    CircuitBreaker.new(accounts[1], accounts[0], 3, 500).
+      then(function(result) { circuit_breaker = result }).
+      then(function() { return Recipient.new(); }).
+      then(function(result) { recipient = result }).
+      then(function() { return web3.eth.sendTransaction({from: accounts[0], to: circuit_breaker.address, value: 1000}, test) }).
+      catch(done);
+  });
+
+  it("should wait the specified number of blocks for cumulative transfers over the limit", function(done) {
+    var circuit_breaker;
+    var recipient;
+
+    var test = function() {
+      circuit_breaker.transfer(300, recipient.address).
+        then(function() { return circuit_breaker.transfer(400, recipient.address); }).
+        then(function() { return circuit_breaker.currentPeriodAmount() }).
+        then(function(result) {
+          assert.equal(result, 300);
+          assert.equal(web3.eth.getBalance(recipient.address), 300);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 700);
+        }).
+        then(function() { return circuit_breaker.releasePendingTransfer(1) }).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 300);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 700);
+        }).
+        then(function() { return circuit_breaker.releasePendingTransfer(1) }).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 300);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 700);
+        }).
+        then(function() { return circuit_breaker.releasePendingTransfer(1) }).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 300);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 700);
+        }).
+        then(function() { return circuit_breaker.releasePendingTransfer(1) }).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 700);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 300);
+          done();
+        }).catch(done);
+    }
+
+    CircuitBreaker.new(accounts[1], accounts[0], 4, 500).
+      then(function(result) { circuit_breaker = result }).
+      then(function() { return Recipient.new(); }).
+      then(function(result) { recipient = result }).
+      then(function() { return web3.eth.sendTransaction({from: accounts[0], to: circuit_breaker.address, value: 1000}, test) }).
+      catch(done);
+  });
+
+  it("should allow the curator to block pending transfers", function(done) {
+    var circuit_breaker;
+    var recipient;
+
+    var test = function() {
+      circuit_breaker.transfer(600, recipient.address).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 0);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 1000);
+        }).
+        then(function() { return circuit_breaker.stopTransfer(0, {from: accounts[1]}); }).
+        then(function() { return circuit_breaker.releasePendingTransfer(0) }).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 0);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 1000);
+        }).
+        then(function() { return circuit_breaker.releasePendingTransfer(0) }).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 0);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 1000);
+          done();
+        }).catch(done);
+    }
+
+    CircuitBreaker.new(accounts[1], accounts[0], 3, 500).
+      then(function(result) { circuit_breaker = result }).
+      then(function() { return Recipient.new(); }).
+      then(function(result) { recipient = result }).
+      then(function() { return web3.eth.sendTransaction({from: accounts[0], to: circuit_breaker.address, value: 1000}, test) }).
+      catch(done);
+  });
+
+  it("should not allow transfers to be double-sent", function(done) {
+    var circuit_breaker;
+    var recipient;
+
+    var test = function() {
+      circuit_breaker.transfer(600, recipient.address).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 0);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 1200);
+        }).
+        then(function() { return circuit_breaker.releasePendingTransfer(0) }).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 0);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 1200);
+        }).
+        then(function() { return circuit_breaker.releasePendingTransfer(0) }).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 0);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 1200);
+        }).
+        then(function() { return circuit_breaker.releasePendingTransfer(0) }).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 600);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 600);
+        }).
+        then(function() { return circuit_breaker.releasePendingTransfer(0) }).
+        then(function() {
+          assert.equal(web3.eth.getBalance(recipient.address), 600);
+          assert.equal(web3.eth.getBalance(circuit_breaker.address), 600);
+          done();
+        }).catch(done);
+    }
+
+    CircuitBreaker.new(accounts[1], accounts[0], 3, 500).
+      then(function(result) { circuit_breaker = result }).
+      then(function() { return Recipient.new(); }).
+      then(function(result) { recipient = result }).
+      then(function() { return web3.eth.sendTransaction({from: accounts[0], to: circuit_breaker.address, value: 1200}, test) }).
+      catch(done);
+  });
+});
